7afd2322705e207abfbc397ba07e273b51db5f6b
Chano <github@chanovera.com>
2024-10-05 14:40:15 -0600

    se habilita el menú mobile

--- assets/js/global.js
+++ assets/js/global.js
@@ -25,3 +25,20 @@ function scrollActions() {
     });
 }
 scrollActions();
+
+// F U N C I O N E S   D E L   M E N Ú   M O B I L E
+
+// abrir el menú mobile
+function openMenuMobile() {
+    // agrega al menú la clase open, mostrándolo
+    document.querySelector('.menu-mobile--wrapper').classList.add('open');
+    // agrega la clase show a una capa que cubre el resto del sitio
+    document.querySelector('#panel-overlay').classList.add('show');
+}
+// cerrar el menú mobile
+function closeMenuMobile() {
+    // quita al menú la clase open, ocultándolo
+    document.querySelector('.menu-mobile--wrapper').classList.remove('open');
+    // quita la clase show a una capa que cubre el resto del sitio
+    document.querySelector('#panel-overlay').classList.remove('show');
+}
