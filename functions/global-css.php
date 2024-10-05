0a67a5fcc422e4d6ebbfa2583c4e14a44a983c21
Chano <github@chanovera.com>
2024-10-05 12:17:02 -0600

    se agregan scripts para agregar clases cuando funciona el scroll

--- functions/global-css.php
+++ functions/global-css.php
@@ -6,10 +6,16 @@ function renata_theme_custom_global_css() {
             /* G E N E R A L E S */
             *,:before,:after{box-sizing:border-box;margin:0;}
             html{scroll-behavior:smooth;}
-            body{font-size:16px;font-weight:400;line-height:1.5;color:var(--wp--preset--color--text);text-align:left;background:var(--wp--preset--color--background);}
+            body{font-size:16px;font-weight:400;line-height:1.5;text-align:left;}
             :is(header,footer,aside) :is(ol,ul){padding-left:0;list-style:none;}
             .container .section{width:min(100% - 30px, 1096px);margin-inline:auto;}
             img{display:block;height:auto;}
+
+            /* C A B E C E R A */
+            #mobile-header{position:sticky;top:0;z-index:2;}
+            #mobile-header .header-content{display:flex;align-items:center;justify-content:space-between;height:54px;}
+            /* estilos al hacer scroll */
+            :is(.scroll-down,.scroll-up) .main-header{;z-index:6;}
         </style>
     <?php
 }
