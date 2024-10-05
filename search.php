2eff00035d788b5cccd4920f39f08390eb1a828f
Chano <github@chanovera.com>
2024-10-05 09:09:20 -0600

    se habilita la página de búsqueda

---
+++ search.php
@@ -0,0 +1,42 @@
+<?php get_header(); ?>
+
+<main>
+
+    <div class="container">
+
+        <section class="section">
+
+            <h1 class="title"><?php echo the_search_query(); ?></h1>
+        
+            <?php
+                if ( have_posts() ){
+
+                    echo '<div class="posts">';
+
+                    while( have_posts() ){
+
+                        the_post();
+                        get_template_part( 'templates/content', 'archive' );
+
+                    }
+
+                    the_posts_pagination();
+
+                    echo '</div>';
+
+                    if ( is_active_sidebar('search-sidebar') ) {
+                        dynamic_sidebar('search-sidebar'); 
+                    } 
+
+                } else {
+                    echo esc_html__('No se han encontrado artículos', 'renata');
+                }
+            ?>
+
+        <section>
+
+    </div>
+
+</main>
+
+<?php get_footer(); ?>
