d3097242b12005468eccabc654ada0cdf741c4f2
Chano <github@chanovera.com>
2024-10-05 09:57:39 -0600

    se agrega la funcionalidad de cargar partes en el footer de wordpress por medio de functions.php

--- functions.php
+++ functions.php
@@ -5,3 +5,10 @@ function load_parts_header() {
     wp_dequeue_style( 'wp-block-library' );
 }
 add_action( 'wp_enqueue_scripts', 'load_parts_header' );
+
+// Carga componentes (estilos, javascript, etc) en el footer
+function load_parts_footer(){
+    // Generales
+    wp_enqueue_script('generals', get_template_directory_uri() . '/assets/js/global.js', array(), '1.0', true);
+}
+add_action( 'get_footer', 'load_parts_footer' );
