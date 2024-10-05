e90cb3358bd5fa5c8202ea99e3fc09be722ee50d
Chano <github@chanovera.com>
2024-10-05 09:56:26 -0600

    se agrega la funcionalidad de cargar partes en el head de wordpress por medio de functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,7 @@
 <?php
+
+// carga componentes (estilos, javascript, etc) en el header
+function load_parts_header() {
+    wp_dequeue_style( 'wp-block-library' );
+}
+add_action( 'wp_enqueue_scripts', 'load_parts_header' );
