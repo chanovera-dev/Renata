319e030f9787358490f07d39419596c4f0b26499
Chano <github@chanovera.com>
2024-10-09 16:55:19 -0600

    estilos css para el header de la plantilla del front-page

--- functions/frontpage-css.php
+++ functions/frontpage-css.php
@@ -8,6 +8,7 @@ function renata_theme_custom_frontpage_css(){
             .page-template-front-page #desktop-header .header-content ul li a{color:#9f9fa0;}
             .page-template-front-page #desktop-header .header-content ul li a:hover{color:#fff;}
             .page-template-front-page #desktop-header .search-form .search-field{background-color:#3d3d42;border:1px solid #3e3d42;color:#929393;}
+            .page-template-front-page #desktop-header .search-form .search-field:focus{color:#fff;}
             .page-template-front-page #desktop-header .search-form .search-submit__button{color:#929393;}
             .page-template-front-page #desktop-header .search-form .search-submit__button:hover{background-color:transparent;color:#fff;}
         </style>
