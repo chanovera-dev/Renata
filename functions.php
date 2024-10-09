48e1affd07f81b07d89109ec43a38b582acfa781
Chano <github@chanovera.com>
2024-10-09 13:32:25 -0600

    quitados los bloqueos a componentes de wordpress

--- functions.php
+++ functions.php
@@ -2,15 +2,12 @@
 
 // carga componentes (estilos, javascript, etc) en el header
 function load_parts_header() {
-    wp_dequeue_style( 'wp-block-library' );
-    wp_deregister_script( 'wp-polyfill' );
-    wp_deregister_script( 'regenerator-runtime' );
+
 }
 add_action( 'wp_enqueue_scripts', 'load_parts_header' );
 
 // Carga componentes (estilos, javascript, etc) en el footer
 function load_parts_footer(){
-    wp_dequeue_style( 'wc-blocks-style' );
     // Generales
     wp_enqueue_script('generals', get_template_directory_uri() . '/assets/js/global.js', array(), '1.0', true);
 }
