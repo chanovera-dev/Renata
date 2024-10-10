d0d6463e179fcaeca9b997d59a347eba6f1f91ba
Chano <github@chanovera.com>
2024-10-10 06:57:36 -0600

    se agrega el portfolio en modo mobile y ajustes css

--- functions/media-queries.php
+++ functions/media-queries.php
@@ -21,11 +21,12 @@ function renata_theme_media_queries() {
                 .blog main .container .section{display:grid;grid-template-columns:1fr 215px;gap:50px;}
                 .page-template-front-page :is(#skills,#portfolio) .content > ul{grid-template-columns:repeat(4, 1fr);gap:30px;}
                 #portfolio.container .content > ul{grid-template-columns:repeat(3, 1fr);}
+                #portfolio.container .content > ul li figure figcaption{position:absolute;top:0;left:0;width:100%;height:100%;background-color:rbg(0 0 0 / 50%);backdrop-filter:saturate(180%) blur(20px);}
             }
 
             @media(min-width:1200px){
                 #portfolio.container .content > ul li figure img{display:inherit;}
-                #portfolio.container .content > ul li figure figcaption{position:absolute;top:0;left:0;width:100%;height:100%;background-color:rbg(0 0 0 / 50%);backdrop-filter:saturate(180%) blur(20px);}
+                
             }
 
         </style>
