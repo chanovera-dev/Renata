2e0d4384f67362267d7b3299f50cb112fb1f5f0f
Chano <github@chanovera.com>
2024-10-09 21:42:09 -0600

    se agrega el portfolio en modo mobile

--- sections/frontpage/portfolio.php
+++ sections/frontpage/portfolio.php
@@ -1,23 +1,93 @@
-<div id="experience" class="container">
-    <section class="section content certifications">
-        <h2>Experience</h2>
-        <div class="card-timeline">
-                <div class="card-item" data-year="February 2017 / Currently">
-                    <div class="card-item-title">Systems manager / Frontend developer / Designer - Freelance.</div>
-                    <div class="card-item-desc">End user support / Design and implementation of websites.</div>
-                </div>
-                <div class="card-item" data-year="June 2016 / February 2017">
-                    <div class="card-item-title">Systems manager in Biss Coworking.</div>
-                    <div class="card-item-desc">Tech support. Security and access control manager. Network management and maintenance manager. Technology Consultant. I standardized facility access protocols, migrated the wireless network to 5Ghz, standardized internal systems to 5Ghz, standardized internal systems and reduced technical failures to 0.</div>
-                </div>
-                <div class="card-item" data-year="July 2012 / December 2012">
-                    <div class="card-item-title">Tech Support in Ciudad del Carmen City Hall.</div>
-                    <div class="card-item-desc">Tech support. Linux-based Institutional Mail Server Administrator.</div>
-                </div>
-                <div class="card-item" data-year="2002 / 2010">
-                    <div class="card-item-title">Systems manager in Bit Byte Computation.</div>
-                    <div class="card-item-desc">Tech support / Teacher.</div>
-                </div>
-            </div>
+<div id="portfolio" class="container">
+    <section class="section content">
+        <h2>Portfolio</h2>
+        <ul>
+            <li>
+                <figure>
+                    <picture>
+                        <source srcset="<?php echo get_theme_mod('img_portfolio1_jpg', get_bloginfo('template_url') . '/assets/img/trueOmniDesktop.jpg'); ?>" type="image/jpeg" media="(width => 1096px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio1_png', get_bloginfo('template_url') . '/assets/img/trueOmniDesktop.png'); ?>" type="image/png" media="(width => 1096px)">
+
+                        <source srcset="<?php echo get_theme_mod('img_portfolio1_jpg', get_bloginfo('template_url') . '/assets/img/trueOmniDesktop.jpg'); ?>" type="image/jpeg" media="(width < 1095px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio1_png', get_bloginfo('template_url') . '/assets/img/trueOmniDesktop.png'); ?>" type="image/png" media="(width < 1096px)">
+
+                        <img src="<?php echo get_theme_mod('img_portfolio1_webp', get_bloginfo('template_url') . '/assets/img/trueOmniDesktop.webp'); ?>" alt="Picture descriptive for True Omni website">
+                    </picture>
+                    <figcaption>
+                        <h3>TrueOmni</h3>
+                        <p>Website for self-service kiosks, digital signage and mobile solutions.</p>
+                        <h4>Design and build with:</h4>
+                        <ul>
+                            <li><i>Wordpress</i></li>
+                            <li><i>Elementor</i></li>
+                        </ul>
+                        <a target="_blank" href="https://www.trueomni.com" class="see-more-button">
+                            See more
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                            </svg>
+                        </a>
+                    </figcaption>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <picture>
+                        <source srcset="<?php echo get_theme_mod('img_portfolio2_jpg', get_bloginfo('template_url') . '/assets/img/pinplastDesktop.jpg'); ?>" type="image/jpeg" media="(width => 1096px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio2_png', get_bloginfo('template_url') . '/assets/img/pinplastDesktop.png'); ?>" type="image/png" media="(width => 1096px)">
+
+                        <source srcset="<?php echo get_theme_mod('img_portfolio2_jpg', get_bloginfo('template_url') . '/assets/img/pinplastDesktop.jpg'); ?>" type="image/jpeg" media="(width < 1095px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio2_png', get_bloginfo('template_url') . '/assets/img/pinplastDesktop.png'); ?>" type="image/png" media="(width < 1096px)">
+
+                        <img src="<?php echo get_theme_mod('img_portfolio2_webp', get_bloginfo('template_url') . '/assets/img/pinplastDesktop.webp'); ?>" alt="Picture descriptive for Pinplast website">
+                    </picture>
+                    <figcaption>
+                        <h3>Pinplast</h3>
+                        <p>Online store of paints and accessories.</p>
+                        <h4>Design and build with:</h4>
+                        <ul>
+                            <li><i>PHP</i></li>
+                            <li><i>Javascript</i></li>
+                            <li><i>Wordpress</i></li>
+                            <li><i>Woocommerce</i></li>
+                        </ul>
+                        <a target="_blank" href="https://www.pinplast.com.mx" class="see-more-button">
+                            See more
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                            </svg>
+                        </a>
+                    </figcaption>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <picture>
+                        <source srcset="<?php echo get_theme_mod('img_portfolio3_jpg', get_bloginfo('template_url') . '/assets/img/revistaEstilo.jpg'); ?>" type="image/jpeg" media="(width => 1096px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio3_png', get_bloginfo('template_url') . '/assets/img/revistaEstilo.png'); ?>" type="image/png" media="(width => 1096px)">
+
+                        <source srcset="<?php echo get_theme_mod('img_portfolio3_jpg', get_bloginfo('template_url') . '/assets/img/revistaEstilo.jpg'); ?>" type="image/jpeg" media="(width < 1095px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio3_png', get_bloginfo('template_url') . '/assets/img/revistaEstilo.png'); ?>" type="image/png" media="(width < 1096px)">
+
+                        <img src="<?php echo get_theme_mod('img_portfolio3_webp', get_bloginfo('template_url') . '/assets/img/revistaEstilo.webp'); ?>" alt="Picture descriptive for Pinplast website">
+                    </picture>
+                    <figcaption>
+                        <h3>Estilo Magazine</h3>
+                        <p>Style magazine in digital format.</p>
+                        <h4>Design and build with:</h4>
+                        <ul>
+                            <li><i>Wordpress</i></li>
+                            <li><i>Elementor</i></li>
+                        </ul>
+                        <a target="_blank" href="https://www.revistaestilo.com.mx" class="see-more-button">
+                            See more
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                            </svg>
+                        </a>
+                    </figcaption>
+                </figure>
+            </li>
+        </ul>
     </section>
 </div>
