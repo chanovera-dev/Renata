c3c25ba566a4320cd7e6291630edbfc891d44ae9
Chano <github@chanovera.com>
2024-10-09 21:13:19 -0600

    se ajusta la rejilla para skills

--- functions/media-queries.php
+++ functions/media-queries.php
@@ -5,16 +5,17 @@ function renata_theme_media_queries() {
         <style>
             #desktop-header{display:none;}
             
-            @media(min-width:769px){
+            @media(min-width:768px){
                 #mobile-header{display:none;}
                 #desktop-header{display:inherit;}
                 #hero.container .introduction{text-align:left;height:calc(100svh - 114px);}
                 .blog main .container .section .posts{grid-template-columns:1fr 1fr;}
-                :is(#skills,#portfolio) .content ul{grid-template-columns:repeat(3, 1fr);gap:30px;}
+                .page-template-front-page :is(#skills,#portfolio) .content ul{grid-template-columns:repeat(3, 1fr);gap:30px;}
             }
 
             @media(min-width:1024px){
                 .blog main .container .section{display:grid;grid-template-columns:1fr 215px;gap:50px;}
+                .page-template-front-page :is(#skills,#portfolio) .content ul{grid-template-columns:repeat(4, 1fr);gap:30px;}
             }
 
         </style>
