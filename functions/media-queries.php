bac5ccd177ba768c2c49871cc9aa661013f4fc9f
Chano <github@chanovera.com>
2024-10-10 07:08:28 -0600

    se agrega el portfolio en modo mobile y ajustes css

--- functions/media-queries.php
+++ functions/media-queries.php
@@ -21,15 +21,12 @@ function renata_theme_media_queries() {
                 .blog main .container .section{display:grid;grid-template-columns:1fr 215px;gap:50px;}
                 .page-template-front-page :is(#skills,#portfolio) .content > ul{grid-template-columns:repeat(4, 1fr);gap:30px;}
                 #portfolio.container .content > ul{grid-template-columns:repeat(3, 1fr);}
-                
-                #portfolio.container .content > ul li figure img{display:block;width:100%;height:100%;object-fit:cover;}
-                #portfolio.container .content > ul li figure figcaption{position:absolute;top:0;left:0;width:100%;height:100%;background-color:rgba(0, 0, 0, .5);backdrop-filter:saturate(180%) blur(20px);opacity:0;transition:opacity .3s ease;}
-                #portfolio.container .content > ul li:hover figure figcaption{opacity:1;}
             }
 
             @media(min-width:1200px){
-                
-                
+                #portfolio.container .content > ul li figure img{display:block;width:100%;height:100%;object-fit:cover;}
+                #portfolio.container .content > ul li figure figcaption{position:absolute;top:0;left:0;width:100%;height:100%;background-color:rgba(0, 0, 0, .5);backdrop-filter:saturate(180%) blur(20px);opacity:0;transition:opacity .3s ease;}
+                #portfolio.container .content > ul li:hover figure figcaption{opacity:1;}
             }
 
         </style>
