ef140449b6595215d4eced7eac76ac80a84b559c
Chano <github@chanovera.com>
2024-10-09 21:38:53 -0600

    se agrega el portfolio en modo mobile

--- front-page.php
+++ front-page.php
@@ -5,6 +5,7 @@
         include(TEMPLATEPATH.'/sections/frontpage/hero.php');
         include(TEMPLATEPATH.'/sections/frontpage/about.php');
         include(TEMPLATEPATH.'/sections/frontpage/skills.php');
+        include(TEMPLATEPATH.'/sections/frontpage/portfolio.php');
     ?>
 </main>
 
