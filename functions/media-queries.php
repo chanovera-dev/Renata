69f0e2c5a1e1d8caaa0be25306c1e3ee4521ad62
Chano <github@chanovera.com>
2024-10-09 19:15:37 -0600

    se agrega la disposición de dos columnas después de 1024px en el contenido

--- functions/media-queries.php
+++ functions/media-queries.php
@@ -3,13 +3,18 @@
 function renata_theme_media_queries() {
     ?>
         <style>
-            #desktop-header{display:none;
-            }
+            #desktop-header{display:none;}
+            
             @media(min-width:769px){
                 #mobile-header{display:none;}
                 #desktop-header{display:inherit;}
                 #hero.container .introduction{text-align:left;height:calc(100svh - 114px);}
             }
+
+            @media(min-width:1024px){
+                main .container .section{display:grid;grid-template-columns:1fr 215px;gap:50px;}
+            }
+
         </style>
     <?php
 }
