0d1ee75e4767679a03b5b5966a3a615343af4412
Chano <github@chanovera.com>
2024-10-10 22:52:59 -0600

    probando el background blur para safari

--- functions/media-queries.php
+++ functions/media-queries.php
@@ -27,7 +27,7 @@ function renata_theme_media_queries() {
                 #portfolio.container .content > ul li figure img{display:block;width:100%;height:100%;object-fit:cover;}
                 #portfolio.container .content > ul li figure figcaption{position:absolute;top:0;left:0;width:100%;height:100%;background-color:rgba(0, 0, 0, .5);backdrop-filter:saturate(180%) blur(20px);opacity:0;transition:opacity .3s ease;}
                 @supports ((-webkit-backdrop-filter: initial) or (backdrop-filter: initial)){
-                    #portfolio.container .content > ul li figure figcaption{--globalnav-background:rgba(22, 22, 23, .8);}
+                    #portfolio.container .content > ul li figure figcaption{background:rgba(22, 22, 23, .8);backdrop-filter:saturate(180%) blur(20px);}
                 }
                 #portfolio.container .content > ul li:hover figure figcaption{opacity:1;}
             }
