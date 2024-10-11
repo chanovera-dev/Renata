3d4f669100fc986d40379083236840ca8f5c3c62
Chano <github@chanovera.com>
2024-10-11 17:14:44 -0600

    se agrega contenido a el portafolio

--- sections/frontpage/portfolio.php
+++ sections/frontpage/portfolio.php
@@ -86,10 +86,10 @@
                 <figure>
                     <picture>
                         <!--
-                        <source srcset="<?php echo get_theme_mod('img_portfolio3_jpg', get_bloginfo('template_url') . '/assets/img/jesusMendez.jpg'); ?>" type="image/jpeg" media="(width => 1200px)">
-                        <source srcset="<?php echo get_theme_mod('img_portfolio3_png', get_bloginfo('template_url') . '/assets/img/jesusMendez.png'); ?>" type="image/png" media="(width => 1200px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio4_jpg', get_bloginfo('template_url') . '/assets/img/jesusMendez.jpg'); ?>" type="image/jpeg" media="(width => 1200px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio4_png', get_bloginfo('template_url') . '/assets/img/jesusMendez.png'); ?>" type="image/png" media="(width => 1200px)">
                         -->
-                        <img src="<?php echo get_theme_mod('img_portfolio3_webp', get_bloginfo('template_url') . '/assets/img/jesusMendez.webp'); ?>" alt="Picture descriptive for Jesus Mendez Fisioteraphy website" loading="lazy">
+                        <img src="<?php echo get_theme_mod('img_portfolio4_webp', get_bloginfo('template_url') . '/assets/img/jesusMendez.webp'); ?>" alt="Picture descriptive for Jesus Mendez Fisioteraphy website" loading="lazy">
                     </picture>
                     <figcaption>
                         <h3>Jesus Mendez physiotherapy</h3>
@@ -101,6 +101,32 @@
                             <li><i>Wordpress</i></li>
                         </ul>
                         <button onclick="window.open('https://www.jesusmendezfisioterapia.com/', '_blank')" class="see-more-button" aria-label="Link to the Jesus Mendez Fisioteraphy website">
+                            See more
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                            </svg>
+                        </button>  
+                    </figcaption>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <picture>
+                        <!--
+                        <source srcset="<?php echo get_theme_mod('img_portfolio5_jpg', get_bloginfo('template_url') . '/assets/img/mujerExpansiva.jpg'); ?>" type="image/jpeg" media="(width => 1200px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio5_png', get_bloginfo('template_url') . '/assets/img/mujerExpansiva.png'); ?>" type="image/png" media="(width => 1200px)">
+                        -->
+                        <img src="<?php echo get_theme_mod('img_portfolio5_webp', get_bloginfo('template_url') . '/assets/img/mujerExpansiva.webp'); ?>" alt="Picture descriptive for Mujer Expansiva website" loading="lazy">
+                    </picture>
+                    <figcaption>
+                        <h3>Mujer Expansiva</h3>
+                        <p>It was created to remind you of your human nature and how important you are on this beautiful planet</p>
+                        <h4>Design and build with:</h4>
+                        <ul>
+                            <li><i>Wordpress</i></li>
+                            <li><i>Divi</i></li>
+                        </ul>
+                        <button onclick="window.open('https://www.mujerexpansiva.com/', '_blank')" class="see-more-button" aria-label="Link to the Jesus Mendez Fisioteraphy website">
                             See more
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
