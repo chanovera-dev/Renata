1bea171a74b4701b95f0bc57f9e2dee7c39a453f
Chano <github@chanovera.com>
2024-10-11 11:29:50 -0600

    se agrega contenido a el portafolio

--- sections/frontpage/portfolio.php
+++ sections/frontpage/portfolio.php
@@ -6,11 +6,8 @@
                 <figure>
                     <picture>
                         <!--
-                        <source srcset="<?php echo get_theme_mod('img_portfolio1_jpg', get_bloginfo('template_url') . '/assets/img/trueOmniDesktop.jpg'); ?>" type="image/jpeg" media="(width => 1096px)">
-                        <source srcset="<?php echo get_theme_mod('img_portfolio1_png', get_bloginfo('template_url') . '/assets/img/trueOmniDesktop.png'); ?>" type="image/png" media="(width => 1096px)">
-
-                        <source srcset="<?php echo get_theme_mod('img_portfolio1_jpg', get_bloginfo('template_url') . '/assets/img/trueOmniDesktop.jpg'); ?>" type="image/jpeg" media="(width < 1095px)">
-                        <source srcset="<?php echo get_theme_mod('img_portfolio1_png', get_bloginfo('template_url') . '/assets/img/trueOmniDesktop.png'); ?>" type="image/png" media="(width < 1096px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio1_jpg', get_bloginfo('template_url') . '/assets/img/trueOmniDesktop.jpg'); ?>" type="image/jpeg" media="(width => 1200px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio1_png', get_bloginfo('template_url') . '/assets/img/trueOmniDesktop.png'); ?>" type="image/png" media="(width => 1200px)">
                         -->
                         <img src="<?php echo get_theme_mod('img_portfolio1_webp', get_bloginfo('template_url') . '/assets/img/trueOmniDesktop.webp'); ?>" alt="Picture descriptive for True Omni website" loading="lazy">
                     </picture>
@@ -35,11 +32,8 @@
                 <figure>
                     <picture>
                         <!-- 
-                        <source srcset="<?php echo get_theme_mod('img_portfolio2_jpg', get_bloginfo('template_url') . '/assets/img/pinplastDesktop.jpg'); ?>" type="image/jpeg" media="(width => 1096px)">
-                        <source srcset="<?php echo get_theme_mod('img_portfolio2_png', get_bloginfo('template_url') . '/assets/img/pinplastDesktop.png'); ?>" type="image/png" media="(width => 1096px)">
-
-                        <source srcset="<?php echo get_theme_mod('img_portfolio2_jpg', get_bloginfo('template_url') . '/assets/img/pinplastDesktop.jpg'); ?>" type="image/jpeg" media="(width < 1095px)">
-                        <source srcset="<?php echo get_theme_mod('img_portfolio2_png', get_bloginfo('template_url') . '/assets/img/pinplastDesktop.png'); ?>" type="image/png" media="(width < 1096px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio2_jpg', get_bloginfo('template_url') . '/assets/img/pinplastDesktop.jpg'); ?>" type="image/jpeg" media="(width => 1200px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio2_png', get_bloginfo('template_url') . '/assets/img/pinplastDesktop.png'); ?>" type="image/png" media="(width => 1200px)">
                         -->
                         <img src="<?php echo get_theme_mod('img_portfolio2_webp', get_bloginfo('template_url') . '/assets/img/pinplastDesktop.webp'); ?>" alt="Picture descriptive for Pinplast website" loading="lazy">
                     </picture>
@@ -66,11 +60,8 @@
                 <figure>
                     <picture>
                         <!--
-                        <source srcset="<?php echo get_theme_mod('img_portfolio3_jpg', get_bloginfo('template_url') . '/assets/img/revistaEstilo.jpg'); ?>" type="image/jpeg" media="(width => 1096px)">
-                        <source srcset="<?php echo get_theme_mod('img_portfolio3_png', get_bloginfo('template_url') . '/assets/img/revistaEstilo.png'); ?>" type="image/png" media="(width => 1096px)">
-
-                        <source srcset="<?php echo get_theme_mod('img_portfolio3_jpg', get_bloginfo('template_url') . '/assets/img/revistaEstilo.jpg'); ?>" type="image/jpeg" media="(width < 1095px)">
-                        <source srcset="<?php echo get_theme_mod('img_portfolio3_png', get_bloginfo('template_url') . '/assets/img/revistaEstilo.png'); ?>" type="image/png" media="(width < 1096px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio3_jpg', get_bloginfo('template_url') . '/assets/img/revistaEstilo.jpg'); ?>" type="image/jpeg" media="(width => 1200px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio3_png', get_bloginfo('template_url') . '/assets/img/revistaEstilo.png'); ?>" type="image/png" media="(width => 1200px)">
                         -->
                         <img src="<?php echo get_theme_mod('img_portfolio3_webp', get_bloginfo('template_url') . '/assets/img/revistaEstilo.webp'); ?>" alt="Picture descriptive for Pinplast website" loading="lazy">
                     </picture>
@@ -83,6 +74,33 @@
                             <li><i>Elementor</i></li>
                         </ul>
                         <button onclick="window.open('https://www.revistaestilo.com.mx/', '_blank')" class="see-more-button" aria-label="Link to the estilo magazine website">
+                            See more
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                            </svg>
+                        </button>  
+                    </figcaption>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <picture>
+                        <!--
+                        <source srcset="<?php echo get_theme_mod('img_portfolio3_jpg', get_bloginfo('template_url') . '/assets/img/jesusMendez.jpg'); ?>" type="image/jpeg" media="(width => 1200px)">
+                        <source srcset="<?php echo get_theme_mod('img_portfolio3_png', get_bloginfo('template_url') . '/assets/img/jesusMendez.png'); ?>" type="image/png" media="(width => 1200px)">
+                        -->
+                        <img src="<?php echo get_theme_mod('img_portfolio3_webp', get_bloginfo('template_url') . '/assets/img/jesusMendez.webp'); ?>" alt="Picture descriptive for Jesus Mendez Fisioteraphy website" loading="lazy">
+                    </picture>
+                    <figcaption>
+                        <h3>Jesus Mendez physiotherapy</h3>
+                        <p>Specialist in minimally invasive techniques for the treatment of various injuries</p>
+                        <h4>Design and build with:</h4>
+                        <ul>
+                            <li><i>PHP</i></li>
+                            <li><i>Javascript</i></li>
+                            <li><i>Wordpress</i></li>
+                        </ul>
+                        <button onclick="window.open('https://www.jesusmendezfisioterapia.com/', '_blank')" class="see-more-button" aria-label="Link to the Jesus Mendez Fisioteraphy website">
                             See more
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
