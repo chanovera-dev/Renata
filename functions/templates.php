da90a547c29d9571fbe77adc7fa1fdec7d56dc92
Chano <github@chanovera.com>
2024-10-10 23:43:49 -0600

    se bloquean componentes de wordpress del frontpage para una carga más rápida

--- functions/templates.php
+++ functions/templates.php
@@ -5,7 +5,7 @@ function renata_frontpage_styles() {
     if ( is_front_page() or is_page_template('front-page.php') ) {
         // carga componentes (estilos, javascript, etc) en el header
         function load_parts_header_frontpage() {
-            wp_dequeue_style( 'wp-block-library' );
+            wp_enqueue_style( 'wp-block-library' );
             wp_deregister_script('wp-polyfill');
             wp_deregister_script('regenerator-runtime');
         }
