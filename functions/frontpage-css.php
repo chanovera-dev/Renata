780d72d4dcc5b04a522532cb3ef907facd443f76
Chano <github@chanovera.com>
2024-10-11 00:12:48 -0600

    se ajustan las medidas de accesibilidad recomendadas por google

--- functions/frontpage-css.php
+++ functions/frontpage-css.php
@@ -11,9 +11,9 @@ function renata_theme_custom_frontpage_css(){
             .page-template-front-page #mobile-header .header-content button{background-color:#33353a;color:#fff;}
             .page-template-front-page #desktop-header .header-content ul li a{color:#9f9fa0;}
             .page-template-front-page #desktop-header .header-content ul li a:hover{color:#fff;}
-            .page-template-front-page #desktop-header .search-form .search-field{background-color:#3d3d42;border:1px solid #3e3d42;color:#929393;}
+            .page-template-front-page #desktop-header .search-form .search-field{background-color:#3d3d42;border:1px solid #3e3d42;color:#f1f1f1;}
             .page-template-front-page #desktop-header .search-form .search-field:focus{color:#fff;}
-            .page-template-front-page #desktop-header .search-form .search-submit__button{color:#929393;}
+            .page-template-front-page #desktop-header .search-form .search-submit__button{color:#f1f1f1;}
             .page-template-front-page #desktop-header .search-form .search-submit__button:hover{background-color:transparent;color:#fff;}
 
             /* hero */
