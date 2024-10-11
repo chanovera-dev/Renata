c33b88640d0fec26e10dc0fdc94d070608b4ab6a
Chano <github@chanovera.com>
2024-10-11 10:23:14 -0600

    se h cambiado el color de fondo del formulario de búsqueda en el frontpage

--- functions/frontpage-css.php
+++ functions/frontpage-css.php
@@ -11,7 +11,7 @@ function renata_theme_custom_frontpage_css(){
             .page-template-front-page #mobile-header .header-content button{background-color:#33353a;color:#fff;}
             .page-template-front-page #desktop-header .header-content ul li a{color:#9198a1;}
             .page-template-front-page #desktop-header .header-content ul li a:hover{color:#fff;}
-            .page-template-front-page #desktop-header .search-form .search-field{background-color:#3d3d42;border:1px solid #3e3d42;color:#f1f1f1;}
+            .page-template-front-page #desktop-header .search-form .search-field{background-color:#212529;border:1px solid #3e3d42;color:#f1f1f1;}
             .page-template-front-page #desktop-header .search-form .search-field:focus{color:#fff;}
             .page-template-front-page #desktop-header .search-form .search-submit__button{color:#f1f1f1;}
             .page-template-front-page #desktop-header .search-form .search-submit__button:hover{background-color:transparent;color:#fff;}
