9880d4b54c903c62b5cd3159b423e1e1761f3860
Chano <github@chanovera.com>
2024-10-10 23:40:52 -0600

    se bloquean componentes de wordpress del frontpage para una carga más rápida

--- functions/templates.php
+++ functions/templates.php
@@ -6,9 +6,15 @@ function renata_frontpage_styles() {
         // carga componentes (estilos, javascript, etc) en el header
         function load_parts_header_frontpage() {
             wp_dequeue_style( 'wp-block-library' );
+            wp_deregister_script('wp-polyfill');
+            wp_deregister_script('regenerator-runtime');
         }
         add_action( 'wp_enqueue_scripts', 'load_parts_header_frontpage' );
         require_once(get_template_directory() . '/functions/frontpage-css.php');
+        function load_components_footer_frontpage(){
+            wp_dequeue_style( 'wc-blocks-style' );
+        }
+        add_action( 'get_footer', 'load_components_footer_frontpage' );
     }
 }
 add_action( 'wp_enqueue_scripts', 'renata_frontpage_styles' );
