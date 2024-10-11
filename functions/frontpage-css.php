88ba62c74c88501ce0004f684cdd6b92a18d94c8
Chano <github@chanovera.com>
2024-10-11 15:27:04 -0600

    ajustes de diseño en experience

--- functions/frontpage-css.php
+++ functions/frontpage-css.php
@@ -82,7 +82,6 @@ function renata_theme_custom_frontpage_css(){
                 box-shadow:0 0 15px #3fb950;
             }
             .certifications .card-item:last-child{padding-bottom:0;}
-            .certifications .card-item:last-child::before{background:transparent;}
             .certifications .card-item-title{font-size:24px;line-height:1.4;}
             .certifications .card-item-desc{color:#9198a1;font-size:16px;margin-top:6px;}
         </style>
