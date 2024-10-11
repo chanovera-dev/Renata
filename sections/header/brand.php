9924143517f79e915ca2d9f5b9684116783a57ba
Chano <github@chanovera.com>
2024-10-11 00:04:31 -0600

    se ajustan las medidas de accesibilidad recomendadas por google

--- sections/header/brand.php
+++ sections/header/brand.php
@@ -1,7 +1,7 @@
 <div class="site-brand">
     <?php
         if ( has_custom_logo() == false ) :
-            echo '<a href="'.get_home_url().'">'.bloginfo('title').'</a>'; 
+            echo '<a href="'.get_home_url().'" aria-label="Link to the home page">'.bloginfo('title').'</a>'; 
         else:
             the_custom_logo();
         endif;
