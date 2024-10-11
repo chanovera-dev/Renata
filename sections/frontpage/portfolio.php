ad0d3e53551ad678d1f7191242e3df7f693a80f6
Chano <github@chanovera.com>
2024-10-11 00:16:55 -0600

    se ajustan las medidas de accesibilidad recomendadas por google

--- sections/frontpage/portfolio.php
+++ sections/frontpage/portfolio.php
@@ -22,7 +22,7 @@
                             <li><i>Wordpress</i></li>
                             <li><i>Elementor</i></li>
                         </ul>
-                        <a target="_blank" href="https://www.trueomni.com" class="see-more-button">
+                        <a target="_blank" href="https://www.trueomni.com" class="see-more-button" aria-label="Link to the trueomni website">
                             See more
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
@@ -53,7 +53,7 @@
                             <li><i>Wordpress</i></li>
                             <li><i>Woocommerce</i></li>
                         </ul>
-                        <a target="_blank" href="https://www.pinplast.com.mx" class="see-more-button">
+                        <a target="_blank" href="https://www.pinplast.com.mx" class="see-more-button" aria-label="Link to the pinplast website">
                             See more
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
@@ -82,7 +82,7 @@
                             <li><i>Wordpress</i></li>
                             <li><i>Elementor</i></li>
                         </ul>
-                        <a target="_blank" href="https://www.revistaestilo.com.mx" class="see-more-button">
+                        <a target="_blank" href="https://www.revistaestilo.com.mx" class="see-more-button" aria-label="Link to the estilo magazine website">
                             See more
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
