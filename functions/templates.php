97cc6577e3fc04337bbe541de6aa3e0671e3857d
Chano <github@chanovera.com>
2024-10-10 23:37:19 -0600

    se bloquea wp-block-library del frontpage

--- functions/templates.php
+++ functions/templates.php
@@ -4,10 +4,10 @@
 function renata_frontpage_styles() {
     if ( is_front_page() or is_page_template('front-page.php') ) {
         // carga componentes (estilos, javascript, etc) en el header
-        function load_parts_header() {
+        function load_parts_header_frontpage() {
             wp_dequeue_style( 'wp-block-library' );
         }
-        add_action( 'wp_enqueue_scripts', 'load_parts_header' );
+        add_action( 'wp_enqueue_scripts', 'load_parts_header_frontpage' );
         require_once(get_template_directory() . '/functions/frontpage-css.php');
     }
 }
