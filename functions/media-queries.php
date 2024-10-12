1bfb17fbc421b891bd05f647fef47064b546cab1
Chano <github@chanovera.com>
2024-10-12 14:31:57 -0600

    ajustes al portafolio

--- functions/media-queries.php
+++ functions/media-queries.php
@@ -12,9 +12,9 @@ function renata_theme_media_queries() {
                 .blog main .container .section .posts{grid-template-columns:1fr 1fr;}
                 #portfolio.container .content > ul,
                 #skills.container .content .cards__inner{grid-template-columns:repeat(3, 1fr);gap:30px;}
-                #portfolio.container .content > ul{grid-template-columns:1fr 1fr;gap:30px;}
+                /* #portfolio.container .content > ul{grid-template-columns:1fr 1fr;gap:30px;} */
                 #skills.container .content ul li{border-radius:10px;}
-                #portfolio.container .content > ul li figure figcaption{padding:30px;}
+                /* #portfolio.container .content > ul li figure figcaption{padding:30px;} */
                 #experience .certifications .card-timeline{padding-left:216px;}
             }
 
@@ -25,9 +25,9 @@ function renata_theme_media_queries() {
             }
 
             @media(min-width:1096px){
-                #portfolio.container .content > ul li figure img{display:block;width:100%;height:100%;object-fit:cover;}
+                /* #portfolio.container .content > ul li figure img{display:block;width:100%;height:100%;object-fit:cover;}
                 #portfolio.container .content > ul li figure figcaption{position:absolute;top:0;left:0;width:100%;height:100%;background-color:rgba(0, 0, 0, .5);backdrop-filter:saturate(180%) blur(20px);-webkit-backdrop-filter:saturate(180%) blur(20px);opacity:0;transition:opacity .3s ease;}
-                #portfolio.container .content > ul li:hover figure figcaption{opacity:1;}
+                #portfolio.container .content > ul li:hover figure figcaption{opacity:1;} */
                 #skills.container .content .cards__inner{grid-template-columns:repeat(5, 1fr);}
             }
 
