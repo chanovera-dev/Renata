f51199fdd6ec6265c01f49cc8999f3acd4f4d210
Chano <github@chanovera.com>
2024-10-12 17:22:26 -0600

    contenido de la página 404

--- 404.php
+++ 404.php
@@ -9,12 +9,12 @@
                 </svg>
             </div>
             <h2><?php echo esc_html__('Page not found.', 'renata'); ?></h2>
-            <a href="<?php site_url(); ?>">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+            <button onclick="window.open('<?php site_url(); ?>')" class="go-home">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                    <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
                 </svg>
                 <?php echo esc_html__('Go to home', 'renata'); ?>
-            </a>
+            </button>
         </section>
     </div>
 </main>
