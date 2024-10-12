c07702be20dde6e753d5c003d6dbc0720d11005b
Chano <github@chanovera.com>
2024-10-12 15:59:27 -0600

    se intenta duplicar el efecto de hover de skills

--- functions/media-queries.php
+++ functions/media-queries.php
@@ -10,7 +10,7 @@ function renata_theme_media_queries() {
                 #desktop-header{display:inherit;}
                 #hero.container .introduction{text-align:left;height:calc(100svh - 114px);}
                 .blog main .container .section .posts{grid-template-columns:1fr 1fr;}
-                /* #portfolio.container .content > ul, */
+                #portfolio.container .content .cards .cards__inner,
                 #skills.container .content .cards__inner{grid-template-columns:repeat(3, 1fr);gap:30px;}
                 /* #portfolio.container .content > ul{grid-template-columns:1fr 1fr;gap:30px;} */
                 #skills.container .content ul li{border-radius:10px;}
