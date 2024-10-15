2de9440de97ed9ce4a327ce28a4c97ba4751fdc9
Chano <github@chanovera.com>
2024-10-15 05:02:38 -0600

    ajustes al diseño del portafolio

--- functions/frontpage-css.php
+++ functions/frontpage-css.php
@@ -122,12 +122,12 @@ function renata_theme_custom_frontpage_css(){
             /* portfolio */
             #portfolio .content{padding-bottom:150px;}
             #portfolio .content > ul{grid-template-columns:1fr;}
-            #portfolio .content > ul > li{overflow:hidden;background-color:#11151A;box-shadow:rgb(255 255 255 / 5%) 0px 1px 1px 0px inset, rgb(0 0 0 / 15%) 0px 50px 100px -20px, rgb(0 0 0 / 30%) 0px 30px 60px -30px;border-radius:10px;padding:15px;}
-            #portfolio .content > ul > li figure{display:grid;gap:16px;height:100%;}
-            #portfolio .content > ul > li figure img{width:100%;aspect-ratio:4/3;object-fit:cover;border-radius:3px;}
-            #portfolio .content > ul li figure figcaption{display:grid;gap:16px;color:#fff;}
+            #portfolio .content > ul > li{overflow:hidden;background-color:#11151A;box-shadow:rgb(255 255 255 / 5%) 0px 1px 1px 0px inset, rgb(0 0 0 / 15%) 0px 50px 100px -20px, rgb(0 0 0 / 30%) 0px 30px 60px -30px;border-radius:10px;padding:5px;}
+            #portfolio .content > ul > li figure{position:relative;display:grid;gap:16px;height:100%;}
+            #portfolio .content > ul > li figure img{width:100%;aspect-ratio:1/1;object-fit:cover;border-radius:6px;}
+            #portfolio .content > ul li figure figcaption{position:absolute;display:grid;gap:16px;color:#fff;background-color:rgb(0 0 0 / 25%);}
             #portfolio .content > ul li figure figcaption h3{font-size:24px;font-weight:400;color:#3fb950;}
-            #portfolio .content > ul li figure figcaption :is(p,h4){color:#9198a1;font-weight:400;}
+            #portfolio .content > ul li figure figcaption p{color:#9198a1;}
             #portfolio .content > ul li figure figcaption ul{margin:0;padding-left:0;list-style:none;}
             #portfolio .content > ul li figure figcaption .see-more-button{margin-inline:auto;padding:7px 12px;display:inline-flex;gap:6px;align-items:center;background-color:#3fb950;border:1px solid #3fb950;border-radius:6px;color:#11151A;}
             #portfolio .content > ul li figure figcaption .see-more-button:hover{cursor:pointer;}
--- sections/frontpage/portfolio.php
+++ sections/frontpage/portfolio.php
@@ -14,19 +14,18 @@
                     <figcaption>
                         <h3>TrueOmni</h3>
                         <p>Website for self-service kiosks, digital signage and mobile solutions.</p>
-                        <h4>Design and build with:</h4>
-                        <ul>
-                            <li>Wordpress</li>
-                            <li>Elementor</li>
-                        </ul>
-                        <button onclick="window.open('https://www.trueomni.com/', '_blank')" class="see-more-button" aria-label="Link to the trueomni website">
-                            See more
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
-                            </svg>
-                        </button>
                     </figcaption>
                 </figure>
+                <ul>
+                    <li>Wordpress</li>
+                    <li>Elementor</li>
+                </ul>
+                <button onclick="window.open('https://www.trueomni.com/', '_blank')" class="see-more-button" aria-label="Link to the trueomni website">
+                    See more
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                    </svg>
+                </button>
             </li>
             <li>
                 <figure>
@@ -40,21 +39,20 @@
                     <figcaption>
                         <h3>Pinplast</h3>
                         <p>Online store of paints and accessories.</p>
-                        <h4>Design and build with:</h4>
-                        <ul>
-                            <li>PHP</li>
-                            <li>Javascript</li>
-                            <li>Wordpress</li>
-                            <li>Woocommerce</li>
-                        </ul>
-                        <button onclick="window.open('https://www.pinplast.com.mx/', '_blank')" class="see-more-button" aria-label="Link to the pinplast website">
-                            See more
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
-                            </svg>
-                        </button>
                     </figcaption>
                 </figure>
+                <ul>
+                    <li>PHP</li>
+                    <li>Javascript</li>
+                    <li>Wordpress</li>
+                    <li>Woocommerce</li>
+                </ul>
+                <button onclick="window.open('https://www.pinplast.com.mx/', '_blank')" class="see-more-button" aria-label="Link to the pinplast website">
+                    See more
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                    </svg>
+                </button>
             </li>
             <li>
                 <figure>
@@ -68,19 +66,18 @@
                     <figcaption>
                         <h3>Estilo Magazine</h3>
                         <p>Style magazine in digital format.</p>
-                        <h4>Design and build with:</h4>
-                        <ul>
-                            <li>Wordpress</li>
-                            <li>Elementor</li>
-                        </ul>
-                        <button onclick="window.open('https://www.revistaestilo.com.mx/', '_blank')" class="see-more-button" aria-label="Link to the estilo magazine website">
-                            See more
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
-                            </svg>
-                        </button>  
                     </figcaption>
                 </figure>
+                <ul>
+                    <li>Wordpress</li>
+                    <li>Elementor</li>
+                </ul>
+                <button onclick="window.open('https://www.revistaestilo.com.mx/', '_blank')" class="see-more-button" aria-label="Link to the estilo magazine website">
+                    See more
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                    </svg>
+                </button>  
             </li>
             <li>
                 <figure>
@@ -94,20 +91,19 @@
                     <figcaption>
                         <h3>Jesus Mendez physiotherapy</h3>
                         <p>Specialist in minimally invasive techniques for the treatment of various injuries</p>
-                        <h4>Design and build with:</h4>
-                        <ul>
-                            <li>PHP</li>
-                            <li>Javascript</li>
-                            <li>Wordpress</li>
-                        </ul>
-                        <button onclick="window.open('https://www.jesusmendezfisioterapia.com/', '_blank')" class="see-more-button" aria-label="Link to the Jesus Mendez Fisioteraphy website">
-                            See more
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
-                            </svg>
-                        </button>  
                     </figcaption>
                 </figure>
+                <ul>
+                    <li>PHP</li>
+                    <li>Javascript</li>
+                    <li>Wordpress</li>
+                </ul>
+                <button onclick="window.open('https://www.jesusmendezfisioterapia.com/', '_blank')" class="see-more-button" aria-label="Link to the Jesus Mendez Fisioteraphy website">
+                    See more
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                    </svg>
+                </button>  
             </li>
             <li>
                 <figure>
@@ -121,19 +117,18 @@
                     <figcaption>
                         <h3>Mujer Expansiva</h3>
                         <p>It was created to remind you of your human nature and how important you are on this beautiful planet</p>
-                        <h4>Design and build with:</h4>
-                        <ul>
-                            <li>Wordpress</li>
-                            <li>Divi</li>
-                        </ul>
-                        <button onclick="window.open('https://www.mujerexpansiva.com/', '_blank')" class="see-more-button" aria-label="Link to the Jesus Mendez Fisioteraphy website">
-                            See more
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
-                            </svg>
-                        </button>  
                     </figcaption>
                 </figure>
+                <ul>
+                    <li>Wordpress</li>
+                    <li>Divi</li>
+                </ul>
+                <button onclick="window.open('https://www.mujerexpansiva.com/', '_blank')" class="see-more-button" aria-label="Link to the Jesus Mendez Fisioteraphy website">
+                    See more
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                    </svg>
+                </button>  
             </li>
         </ul>
     </section>
