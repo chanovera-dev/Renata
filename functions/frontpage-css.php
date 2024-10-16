7302549394ef77d481c14751c283099757fe59e3
Chano <github@chanovera.com>
2024-10-16 14:16:48 -0600

    estilos para la sección de contacto

--- functions/frontpage-css.php
+++ functions/frontpage-css.php
@@ -177,7 +177,7 @@ function renata_theme_custom_frontpage_css(){
             #contact .section .contact-form p button{margin-right:auto;font-family:'Roboto', sans-serif;font-size:16px;padding:10px 20px;border:1px solid #323232;border-radius:4px;background-color:transparent;color:#323232;transition:all .3s ease;}
             #contact .section .contact-form p button:hover{background-color:#323232;color:#fff;cursor:pointer;}
             #contact .section .contact-form p button:focus{outline:none;background-color:orange;color:#fff;border-color:orange;}
-            #contact .section .contact-form p :is(button.disabled,button.disabled:hover){border:1px solid #ccc;color:#ccc;background-color:#f6f6f6;cursor:auto;}
+            #contact .section .contact-form p :is(button.disabled,button.disabled:hover){border:1px solid #11151A;color:#11151A;background-color:transparent;cursor:auto;}
             #contact .section ul{padding-left:0;margin:0;list-style:none;display:grid;}
             #contact .section ul li{display:inline-flex;align-items:center;gap:10px;height:38px;color:#fff;}
             #contact .section ul li a{text-decoration:none;display:inline-flex;align-items:center;gap:10px;color:#fff;border-radius:2px;transition:all .3s ease;}
