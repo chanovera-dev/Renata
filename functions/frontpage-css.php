c54c391f6d0208843ce249a686d4e779805ca029
Chano <github@chanovera.com>
2024-10-16 14:31:10 -0600

    estilos para hero

--- functions/frontpage-css.php
+++ functions/frontpage-css.php
@@ -37,7 +37,7 @@ function renata_theme_custom_frontpage_css(){
             }
             #hero .introduction :is(h1, h2){font-weight:400;font-size:36px;line-height:1.4;color:#fff;}
             #hero .introduction h2 span{font-size:42px;color:#3fb950;}
-            #hero .introduction p{padding:3px 5px;background-color:#3fb950;border:1px solid #3fb950;border-radius:4px;color:#11151A;margin-bottom:24px;font-size:13px;display:inline-block;}
+            #hero .introduction p{padding:3px 7px;background-color:#26303b;border:1px solid #26303b;border-radius:4px;color:#d1d1d1;margin-bottom:24px;font-size:13px;display:inline-block;}
             #hero .introduction .contact-me{padding:10px 20px;border:2px solid #3fb950;border-radius:6px;background-color:transparent;color:#3fb950;font-size:16px;font-weight:600;margin-top:36px;display:flex;align-items:center;gap:10px;}
             #hero .introduction .contact-me svg{height:1cap;aspect-ratio:1;}
 
