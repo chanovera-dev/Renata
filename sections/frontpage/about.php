6e2027cb5b274eb4165a7ee6a69612b4fad4e0df
Chano <github@chanovera.com>
2024-10-16 06:55:19 -0600

    se actualiza el texto de about y de experience

--- sections/frontpage/about.php
+++ sections/frontpage/about.php
@@ -2,9 +2,9 @@
     <section class="section content">
         <h2>About</h2>
         <div>
-            <p>I specialize in the development of custom websites based on WordPress, covering a wide range of projects including e-commerce stores, blogs, and professional service websites. Each project is tailored to meet the specific needs of each client, ensuring a solution that aligns with their goals and expectations.</p>
-            <p>My approach is focused on continuous improvement, allowing me to deliver high-quality products that not only meet current market standards but also provide an optimized user experience, ensuring both functionality and visual appeal.</p>
-            <p>I am committed to providing a comprehensive service, from initial planning and design to implementation and ongoing maintenance, ensuring a final product that delivers real value to each client's business.</p>
+            <p>I am a web developer specializing in custom WordPress websites, covering a wide range of projects such as e-commerce stores, blogs, and professional service websites. Each project is tailored to the specific needs of each client, ensuring a solution that aligns with their goals and expectations.</p>
+            <p>My approach is based on continuous improvement, allowing me to deliver high-quality products that not only meet current market standards but also provide an optimized user experience, ensuring both functionality and visual appeal.</p>
+            <p>I am committed to providing a comprehensive service—from initial planning and design to implementation and ongoing maintenance—to ensure a final product that delivers real value to each client’s business.</p>
         </div>
     </section>
 </div>
--- sections/frontpage/experience.php
+++ sections/frontpage/experience.php
@@ -3,20 +3,20 @@
         <h2>Experience</h2>
         <div class="card-timeline">
                 <div class="card-item" data-year="February 2017 / Currently">
-                    <div class="card-item-title">Systems manager / Frontend developer / Designer - Freelance.</div>
-                    <div class="card-item-desc">End user support / Design and implementation of websites.</div>
+                    <div class="card-item-title">Systems Manager / Frontend Developer / Designer - Freelance.</div>
+                    <div class="card-item-desc">Provided end-user support and managed the design and implementation of websites.</div>
                 </div>
                 <div class="card-item" data-year="June 2016 / February 2017">
                     <div class="card-item-title">Systems manager in Biss Coworking.</div>
-                    <div class="card-item-desc">Tech support. Security and access control manager. Network management and maintenance manager. Technology Consultant. I standardized facility access protocols, migrated the wireless network to 5Ghz, standardized internal systems to 5Ghz, standardized internal systems and reduced technical failures to 0.</div>
+                    <div class="card-item-desc">Oversaw tech support, security, and access control, as well as network management and maintenance. As a Technology Consultant, I standardized facility access protocols, migrated the wireless network to 5GHz, streamlined internal systems, and reduced technical failures to zero.</div>
                 </div>
                 <div class="card-item" data-year="July 2012 / December 2012">
                     <div class="card-item-title">Tech Support in Ciudad del Carmen City Hall.</div>
-                    <div class="card-item-desc">Tech support. Linux-based Institutional Mail Server Administrator.</div>
+                    <div class="card-item-desc">Provided technical support and served as the Administrator for the Linux-based Institutional Mail Server.</div>
                 </div>
                 <div class="card-item" data-year="2002 / 2010">
                     <div class="card-item-title">Systems manager in Bit Byte Computation.</div>
-                    <div class="card-item-desc">Tech support / Teacher.</div>
+                    <div class="card-item-desc">Managed tech support services and worked as an instructor.</div>
                 </div>
             </div>
     </section>
