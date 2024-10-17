f3d00a00242aafeea047bb30c891892c257286c4
Chano <github@chanovera.com>
2024-10-17 09:07:08 -0600

    se crea un condicional sobre si hay imagen destacada o no en el loop

--- templates/content-archive.php
+++ templates/content-archive.php
@@ -1,5 +1,10 @@
 <article>
-    <img class="thumbnail" src="<?php the_post_thumbnail_url( 'media' ); ?>" alt="Imagen del artículo" loading="lazy" width="300" height="200">
+    <?php
+        if ( has_post_thumbnail() == false ) :
+        else:
+            echo '<img class="thumbnail" src="'; the_post_thumbnail_url( 'media' ); echo '" alt="Imagen del artículo" loading="lazy" width="300" height="200">';
+        endif;
+    ?>
     <a href="<?php the_permalink(); ?>" target="_blank"><?php the_title('<h3>', '</h3>'); ?></a>
     <p><?php the_excerpt(); ?></p>
 </article>
