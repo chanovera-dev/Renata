26f5fa9f61e121614c007cb65a05a2a9bf0b81de
Chano <github@chanovera.com>
2024-10-17 06:54:10 -0600

    se personaliza la salida de wp_list_categories()

--- functions.php
+++ functions.php
@@ -141,26 +141,7 @@ function custom_archives_link($link_html, $url, $text, $format, $before, $after)
 }
 add_filter('get_archives_link', 'custom_archives_link', 10, 6);
 
-// Personaliza la salida de las categorías en el widget
-function custom_category_links($output, $args) {
-    // Reemplaza cada enlace de categoría
-    $output = preg_replace_callback(
-        '/<a href="([^"]+)">([^<]+)<\/a>/',
-        function ($matches) {
-            // Crea el nuevo enlace con el SVG
-            return '<li><a href="' . esc_url($matches[1]) . '">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark" viewBox="0 0 16 16">
-                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
-            </svg>
-            ' . esc_html($matches[2]) . '</a></li>';
-        },
-        $output
-    );
-
-    return $output;
-}
-add_filter('wp_list_categories', 'custom_category_links', 10, 2);
-
+// personaliza la salida de las categorias en el widget
 function custom_category_list($output, $args) {
     $categories = get_categories($args);
 
