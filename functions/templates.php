33befb272debe071109db8fae75c5bc24367de21
Chano <github@chanovera.com>
2024-10-16 20:38:45 -0600

    primeros estilos para las sidebars

--- functions/templates.php
+++ functions/templates.php
@@ -19,3 +19,11 @@ function renata_frontpage_styles() {
     }
 }
 add_action( 'wp_enqueue_scripts', 'renata_frontpage_styles' );
+
+// Estilos para la página archivo                                                           
+function karlicius_posts_styles() {
+    if ( is_home() or is_page_template('home.php') or is_archive() or is_search() ) {
+        wp_enqueue_style( 'sidebar-styles', get_template_directory_uri() . '/assets/css/sidebar.css' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'karlicius_posts_styles' );
