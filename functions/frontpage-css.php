b13b93de07f2c91ba808a90e87ccc429d7e59933
Chano <github@chanovera.com>
2024-10-17 15:33:45 -0600

    editando las animaciones del hero

--- functions/frontpage-css.php
+++ functions/frontpage-css.php
@@ -44,15 +44,17 @@ function renata_theme_custom_frontpage_css(){
             /* hero */
             #hero{position:relative;background-color:#11151A;color:#fff;box-shadow:0 18px 20px 0 rgb(0 0 0 / 15%);}
             #hero .section{display:grid;}
-            #hero .introduction{
-                min-height:400px;height:calc(100svh - 54px);max-height:1000px;place-content:center;text-align:center;opacity:1;
-                animation-name:animation-introduction; animation-direction:normal; animation-duration:1.5s; animation-timing-function:ease; animation-iteration-count:1;
-            }
+            #hero .introduction{min-height:400px;height:calc(100svh - 54px);max-height:1000px;place-content:center;text-align:center;}
             @keyframes animation-introduction{
                 0%   {opacity:0;transform:translateY(60px);}
                 100% {opacity:1;transform:translateY(0);}
             }
             #hero .introduction :is(h1, h2){font-weight:400;font-size:36px;line-height:1.4;color:#fff;}
+            #hero .introduction h2{opacity:1;animation-name:animation-introduction; animation-direction:normal; animation-duration:1.5s; animation-timing-function:ease; animation-iteration-count:1;}
+            @keyframes animation-introduction{
+                0%   {opacity:0;transform:translateY(60px);}
+                100% {opacity:1;transform:translateY(0);}
+            }
             #hero .introduction h2 span{font-size:42px;color:#3fb950;}
             #hero .introduction p{padding:3px 7px;background-color:#26303b;border:1px solid #26303b;border-radius:4px;color:#d1d1d1;margin-bottom:24px;font-size:13px;display:inline-block;}
             #hero .introduction .contact-me{margin-inline:auto;padding:6px 16px;border:2px solid #3fb950;border-radius:6px;background-color:#3fb950;color:#11151A;font-size:20px;margin-top:30px;display:flex;align-items:center;gap:10px;transition:all .3s ease;}
