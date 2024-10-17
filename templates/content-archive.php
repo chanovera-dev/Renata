87a64c6fe7336b517f4bd453f1c6c952f981768e
Chano <github@chanovera.com>
2024-10-17 08:05:51 -0600

    se agregan las miniaturas del artículo al loop de wordpress

--- templates/content-archive.php
+++ templates/content-archive.php
@@ -1,5 +1,5 @@
 <article>
-    <img class="thumbnail" src="'; the_post_thumbnail_url( 'media' ); echo '" alt="Imagen del artículo" loading="lazy" width="300" height="200">
+    <img class="thumbnail" src="<?php the_post_thumbnail_url( 'media' ); ?>" alt="Imagen del artículo" loading="lazy" width="300" height="200">
     <a href="<?php the_permalink(); ?>" target="_blank"><?php the_title('<h3>', '</h3>'); ?></a>
     <p><?php the_excerpt(); ?></p>
 </article>
