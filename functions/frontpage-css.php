dcac23a78dfbe652bf94b443e827a07720982b65
Chano <github@chanovera.com>
2024-10-17 15:01:45 -0600

    separación de efectos de entrada en los componentes del header

--- functions/frontpage-css.php
+++ functions/frontpage-css.php
@@ -11,7 +11,7 @@ function renata_theme_custom_frontpage_css(){
                 opacity:1;
                 animation-name:animation-header-1; animation-direction:normal; animation-duration:1.5s; animation-timing-function:ease; animation-iteration-count:1;
             }
-            .page-template-front-page #mobile-header .site-brand{animation-name:animation-header-2;animation-duration:2s;}
+            .page-template-front-page #mobile-header .site-brand{animation-name:animation-header-2;}
             .page-template-front-page #mobile-header .open-searchform-mobile--button{animation-name:animation-header-3;}
             @keyframes animation-header-1{
                 0%    {opacity:0;transform:translateY(-60px);}
