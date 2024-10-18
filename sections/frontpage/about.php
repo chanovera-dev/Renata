239386eb742fbc98960df261a65001c26341480f
Chano <github@chanovera.com>
2024-10-18 07:59:37 -0600

    se resume el texto de about

--- sections/frontpage/about.php
+++ sections/frontpage/about.php
@@ -2,9 +2,7 @@
     <section class="section content">
         <h2>About</h2>
         <div>
-            <p>I am a web developer specializing in custom WordPress websites, covering a wide range of projects such as e-commerce stores, blogs, and professional service websites. Each project is tailored to the specific needs of each client, ensuring a solution that aligns with their goals and expectations.</p>
-            <p>My approach is based on continuous improvement, allowing me to deliver high-quality products that not only meet current market standards but also provide an optimized user experience, ensuring both functionality and visual appeal.</p>
-            <p>I am committed to providing a comprehensive service—from initial planning and design to implementation and ongoing maintenance—to ensure a final product that delivers real value to each client’s business.</p>
+            <p>I am a web developer specializing in custom WordPress sites, creating solutions tailored to each client's needs, such as online stores and blogs. My focus on continuous improvement ensures high-quality products that optimize the user experience. I provide a comprehensive service, from planning to maintenance, to ensure that each project adds real value to the business.</p>
         </div>
     </section>
 </div>
