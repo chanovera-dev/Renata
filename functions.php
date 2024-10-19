40f5d17c1143305356d06b9133cfb0bcb1138a40
Chano <github@chanovera.com>
2024-10-19 16:50:29 -0600

    evitar que el menú mobile se cierre al abrir un submenú

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ add_action( 'wp_enqueue_scripts', 'load_parts_header' );
 // Carga componentes (estilos, javascript, etc) en el footer
 function load_parts_footer(){
     // Generales
-    wp_enqueue_script('generals', get_template_directory_uri() . '/assets/js/global.min.js', array(), '1.0', true);
+    wp_enqueue_script('generals', get_template_directory_uri() . '/assets/js/global.js', array(), '1.0', true);
 }
 add_action( 'get_footer', 'load_parts_footer' );
 
