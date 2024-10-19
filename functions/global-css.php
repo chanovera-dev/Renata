93ad56d9b8f429766b67895006656d3c368f23a1
Chano <github@chanovera.com>
2024-10-19 17:52:15 -0600

    estilos para la búsqueda mobile

--- functions/global-css.php
+++ functions/global-css.php
@@ -80,8 +80,7 @@ function renata_theme_custom_global_css() {
             #searchform-mobile.open{top:0;box-shadow:var(--wp--preset--shadow--header);}
             #searchform-mobile > div{display:flex;align-items:center;margin-inline:auto;}
                 /* formulario */
-                #searchform-mobile input[type=text]{width:min(100% - 88px);height:34px;border:0 solid transparent;border-radius:6px;background-color:var(--wp--preset--color--bg-panel);padding:8px 15px 8px 10px;font-family:'Roboto';font-size:16px;}
-                #searchform-mobile input[type=text]:focus{outline:none;}
+                #searchform-mobile input[type=text]{width:min(100% - 88px);}
                 /* botones */
                 #searchform-mobile .buttons-container{display:flex;align-items:center;padding:10px 0 10px 10px;gap:10px;}
                 #searchform-mobile .buttons-container :is(button,.close-searchform-mobile){display:grid;place-content:center;width:34px;height:34px;border:none;background-color:var(--wp--preset--color--white);color:var(--wp--preset--color--text);border-radius:6px;transition:all .3s ease;}
