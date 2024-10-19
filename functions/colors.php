972190c51aa8c7cdbf5288e5376b29d0063f9217
Chano <github@chanovera.com>
2024-10-19 13:48:01 -0600

    se agrega soporte especializado para los formularios

--- functions/colors.php
+++ functions/colors.php
@@ -17,8 +17,8 @@ function renata_theme_colors() {
                 /* formularios */
                 --bg-input:#e6ebef;
                 --border-input:1px solid #dbe1e8;
-                --border-input-hover:#c3cdd8;
-                --border-color-focus:#b7c3d0;
+                --border-input-hover:#cfd7e0;
+                --border-color-focus:#c3cdd8;
                 --color-input:#3D4651;
                 --color-placeholder:#9fafc1;
             }
@@ -39,8 +39,8 @@ function renata_theme_colors() {
                 /* formularios */
                 --bg-input:#26303b;
                 --border-input:1px solid #2e3a47;
-                --border-input-hover:#3d4d5f;
-                --border-color-focus:#45576b;
+                --border-input-hover:#354353;
+                --border-color-focus:#3d4d5f;
                 --color-input:#f6f6f6;
                 --color-placeholder:#546a83;
             }
