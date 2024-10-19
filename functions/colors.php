151b9b02d616cab13400799a4f9a4fd7b07ff272
Chano <github@chanovera.com>
2024-10-19 12:51:24 -0600

    pruebas para fondo de cabecera en modo oscuro

--- functions/colors.php
+++ functions/colors.php
@@ -4,23 +4,37 @@ function renata_theme_colors() {
     ?>
     <style>
         :root {
-                --bg-header:rgb(255 255 255 / 100%);
+                /* generales */
                 --white:#f6f6f6;
                 --color-title:#3D4651;
                 --color-text:#3D4651;
                 --color-link:#3D4651;
                 --color-link-hover:#000;
+
+                /* cabecera */
+                --bg-header:rgb(255 255 255 / 100%);
+
+                /* formularios */
+                --bg-input:#e6ebef;
+                --border-input:1px solid #dbe1e8;
             }
 
         /* Modo oscuro */
         @media (prefers-color-scheme: dark) {
             :root{
-                --bg-header:rgb(17 21 26 / 100%);
+                /* generales */
                 --white:#323232;
                 --color-title:#9198a1;
                 --color-text:#9198a1;
                 --color-link:#9198a1;
                 --color-link-hover:#fff;
+
+                /* cabecera */
+                --bg-header:rgb(17 21 26 / 100%);
+
+                /* formularios */
+                --bg-input:#26303b;
+                --border-input:1px solid #5c748f;
             }
         }
     </style>
