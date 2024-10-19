bc69fea2dc7196f714b6cc1b5933e8fe95f9851f
Chano <github@chanovera.com>
2024-10-19 16:11:44 -0600

    ajustes al menú

--- functions/global-css.php
+++ functions/global-css.php
@@ -51,7 +51,7 @@ function renata_theme_custom_global_css() {
             #menu-mobile{height:100%;overflow-y:scroll;padding-bottom:60px;}
             #menu-mobile::-webkit-scrollbar{display:none;}
             #menu-mobile ul{padding:0;list-style:none;}
-            #menu-mobile ul li a:has(button){padding:0!important;width:0!important;}
+            #menu-mobile ul li a:has(button){padding:0!important;}
             #menu-mobile ul li a:has(button) button{display:flex;align-items:center;justify-content:space-between;}
             #menu-mobile ul li :is(a, button){display:flex;width:100%;padding:12px 20px;background-color:transparent;font-size:16px;font-family:'Roboto';color:var(--wp--preset--color--text);border:none;transition:background-color .3s ease;}
             #menu-mobile ul li :is(a:hover, button:hover){background-color:var(--bg-color-link-hover);}
