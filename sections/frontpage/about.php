78863a729fe2c59773b0f301cd72a0d6768ee67b
Chano <github@chanovera.com>
2024-10-19 09:35:12 -0600

    se divide el párrafo para hacer contenido más pequeño y evitar la penalización de google

--- sections/frontpage/about.php
+++ sections/frontpage/about.php
@@ -2,7 +2,8 @@
     <section class="section content">
         <h2>About</h2>
         <div>
-            <p>I am a web developer specializing in custom WordPress sites, creating solutions tailored to each client's needs, such as online stores and blogs. My focus on continuous improvement ensures high-quality products that optimize the user experience. I provide a comprehensive service, from planning to maintenance, to ensure that each project adds real value to the business.</p>
+            <p>I am a web developer specializing in custom WordPress sites, creating solutions tailored to each client's needs, such as online stores and blogs.</p>
+            <p>My focus on continuous improvement ensures high-quality products that optimize the user experience. I provide a comprehensive service, from planning to maintenance, to ensure that each project adds real value to the business.</p>
         </div>
     </section>
 </div>
