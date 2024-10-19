d8d9812b72c7664981be9ca9933ce08df645a85d
Chano <github@chanovera.com>
2024-10-19 09:46:45 -0600

    se separan las secciones en los media queries

--- functions/media-queries.php
+++ functions/media-queries.php
@@ -6,26 +6,45 @@ function renata_theme_media_queries() {
             #desktop-header{display:none;}
             
             @media(min-width:768px){
+                /* cabecera */
                 #mobile-header{display:none;}
                 #desktop-header{display:inherit;}
+
+                /* hero */
                 #hero.container .introduction{text-align:left;height:calc(100svh - 114px);}
                 #hero.container .introduction .contact-me{margin-inline:inherit;}
+
+                /* blog */
                 .blog main .container .section .posts{grid-template-columns:1fr 1fr;}
+
+                /* portafolio */
                 #portfolio.container .content > ul{grid-template-columns:1fr 1fr;}
+
+                /* skills */
                 #skills.container .content .cards__inner{grid-template-columns:repeat(3, 1fr);gap:30px;}
                 #skills.container .content ul li{border-radius:10px;}
+
+                /* experiencia */
                 #experience .certifications .card-timeline{padding-left:216px;}
+
+                /* contacto */
                 #contact.container .section{grid-template-columns:auto 250px;width:min-content;}
                 #contact.container .section > div:nth-child(1){min-width:400px;}
             }
 
             @media(min-width:1024px){
+                /* blog */
                 .blog main .container .section{display:grid;grid-template-columns:1fr 215px;gap:50px;}
+
+                /* skills */
                 #skills.container .content .cards__inner{grid-template-columns:repeat(4, 1fr);gap:30px;}
+
+                /* portafolio */
                 #portfolio.container .content > ul{grid-template-columns:repeat(3, 1fr);gap:30px;}
             }
 
             @media(min-width:1096px){
+                /* skills */
                 #skills.container .content .cards__inner{grid-template-columns:repeat(5, 1fr);}
             }
 
