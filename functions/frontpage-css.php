4a98425c3f046c28920ce8111bca0bc4d433504d
Chano <github@chanovera.com>
2024-10-19 20:12:05 -0600

    otros colores de modo día y noche

--- functions/frontpage-css.php
+++ functions/frontpage-css.php
@@ -165,7 +165,7 @@ function renata_theme_custom_frontpage_css(){
             #portfolio .content > ul li > div .see-more-button:hover{cursor:pointer;background-color:#fff;}
 
             /* experience */
-            #experience{position:relative;background-color:var(--bg-header-experience);color:var(--resalted-text);box-shadow:0 -18px 20px 0 rgb(0 0 0 / 15%);}
+            #experience{position:relative;background-color:var(--bg-hero-experience);color:var(--resalted-text);box-shadow:0 -18px 20px 0 rgb(0 0 0 / 15%);}
             #experience .content{padding:57px 0 70px;}
             #experience .content h2{font-size:36px;font-family:'Text me one';position:relative;color:var(--resalted-text);}
             .certifications .card-timeline{position:relative;display:grid;padding-left:4px;}
