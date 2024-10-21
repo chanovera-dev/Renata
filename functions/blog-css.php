ab21a7966ded9ff0440ff307534450db275a114f
Chano <github@chanovera.com>
2024-10-21 13:22:36 -0600

    estableciendo el espaciado entre los posts y el aside

--- functions/blog-css.php
+++ functions/blog-css.php
@@ -3,16 +3,14 @@
 function renata_theme_custom_blog_css() {
     ?>
     <style>
-        .blog main .container{background-color:var(--bg-header);}
-        .blog main .container .section{padding:30px 0;display:grid;gap:50px;}
-        .blog main .container .section .posts-wrapper{display:grid;grid-template-rows:1fr auto;gap:50px;}
-        .blog main .container .section .posts{display:grid;gap:50px;align-content:baseline;grid-template-columns:repeat(auto-fill, minmax(291px, 1fr));}
-
-        /* blog */
-        .blog main .container .section .posts .post{display:grid;gap:16px;align-content:baseline;}
-        .blog main .container .section .posts .post .thumbnail{width:100%;aspect-ratio:16/9;object-fit:cover;background-color:var(--bg-input);}
-        .blog main .container .section .posts .post .permalink .permalink__title{}
-        .blog main .container .section .posts .post .excerpt{}
+        main .container{background-color:var(--bg-header);}
+        main .container .section{padding:30px 0;display:grid;gap:50px;}
+        main .container .section .posts-wrapper{display:grid;grid-template-rows:1fr auto;gap:50px;}
+        main .container .section .posts-wrapper .posts{display:grid;gap:50px;align-content:baseline;grid-template-columns:repeat(auto-fill, minmax(291px, 1fr));}
+        main .container .section .posts-wrapper .posts .post{display:grid;gap:16px;align-content:baseline;}
+        main .container .section .posts-wrapper .posts .post .thumbnail{width:100%;aspect-ratio:16/9;object-fit:cover;background-color:var(--bg-input);}
+        main .container .section .posts-wrapper .posts .post .permalink .permalink__title{}
+        main .container .section .posts-wrapper .posts .post .excerpt{}
     </style>
     <?php
 }
