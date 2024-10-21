0dbf81db2097d6cec395902dfcb4542884ec9024
Chano <github@chanovera.com>
2024-10-20 19:45:16 -0600

    asignación de clases en el loop de wordpress

--- templates/content-archive.php
+++ templates/content-archive.php
@@ -5,6 +5,6 @@
             echo '<img class="thumbnail" src="'; the_post_thumbnail_url( 'media' ); echo '" alt="Imagen del artículo" loading="lazy" width="300" height="200">';
         endif;
     ?>
-    <a href="<?php the_permalink(); ?>" target="_blank"><?php the_title('<h3>', '</h3>'); ?></a>
-    <p><?php echo get_the_excerpt(); ?></p>
+    <a class="permalink" href="<?php the_permalink(); ?>" target="_blank"><?php the_title('<h3 class="permalink__title">', '</h3>'); ?></a>
+    <p class="excerpt"><?php echo get_the_excerpt(); ?></p>
 </article>
