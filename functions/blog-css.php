b606c2a9631e0cf7db0ac0570287fa5e62bf6e59
Chano <github@chanovera.com>
2024-10-20 19:41:26 -0600

    asignación de clases en el loop de wordpress

--- functions/blog-css.php
+++ functions/blog-css.php
@@ -8,7 +8,7 @@ function renata_theme_custom_blog_css() {
         .blog main .container .section .posts{display:grid;gap:30px;}
 
         /* blog */
-        .blog main .container .section .posts .post > *:not(:last-child){margin-bottom:20px;}
+        .blog main .container .section .posts .post{display:grid;gap:20px;}
         .blog main .container .section .posts .post img{width:100%;aspect-ratio:4/3;object-fit:cover;}
     </style>
     <?php
