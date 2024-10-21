904a250802aa12c79f8118c6117be6466a68f585
Chano <github@chanovera.com>
2024-10-20 19:25:13 -0600

    se elimina la personalización del excerpt

--- functions.php
+++ functions.php
@@ -127,15 +127,6 @@ function widgets_areas(){
 }
 add_action( 'widgets_init', 'widgets_areas' );
 
-// Delimita el tamaño del excerpt a 25 palabras
-function limite_excerpt($limite) { return 25; }
-add_filter ('excerpt_length', 'limite_excerpt', 999);
-
-function eliminar_p_vacios($content) {
-    return preg_replace('/<p>\s*<\/p>/', '', $content);
-}
-add_filter('the_excerpt', 'eliminar_p_vacios');
-
 // salida de wp_archive_list() personalizada
 function custom_archives_link($link_html, $url, $text, $format, $before, $after) {
     // Modify the $link_html to customize the link structure
