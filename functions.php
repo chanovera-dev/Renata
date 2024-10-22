38b566d236ffe50d8b0326e7e04eeabf84b1befb
Chano <github@chanovera.com>
2024-10-21 19:04:00 -0600

    personalizar la salida de la lista de etiquetas

--- functions.php
+++ functions.php
@@ -153,6 +153,26 @@ function custom_category_list($output, $args) {
 }
 add_filter('wp_list_categories', 'custom_category_list', 10, 2);
 
+// personaliza la salida de las etiquetas
+$tags = get_the_tags();
+if ($tags) {
+    $tags_list = '';
+    // SVG como una variable
+    $svg_icon = '
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag" viewBox="0 0 16 16">
+            <path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0"/>
+            <path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1m0 5.586 7 7L13.586 9l-7-7H2z"/>
+        </svg>
+    ';
+
+    foreach ($tags as $tag) {
+        $tags_list .= '<a href="' . esc_url(get_tag_link($tag->term_id)) . '" class="custom-tag">' . $svg_icon . esc_html($tag->name) . '</a>, ';
+    }
+    // Eliminar la última coma y espacio
+    $tags_list = rtrim($tags_list, ', ');
+
+    echo '<div class="tags-list">Etiquetas: ' . $tags_list . '</div>';
+}
 
 // A N E X O S
 /* anexo para cargar el css que se usa en todas las páginas */
