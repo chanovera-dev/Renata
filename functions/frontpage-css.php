119431a9931f1a9a5bbb53ab7d8659ab3e6dbdd2
Chano <github@chanovera.com>
2024-10-22 07:51:44 -0600

    probando animación de entrada para las tarjetas de portafolio

--- functions/frontpage-css.php
+++ functions/frontpage-css.php
@@ -160,7 +160,7 @@ function renata_theme_custom_frontpage_css(){
                     transform-origin: top;
                     opacity: 0;
                 }
-                50% {
+                25% {
                     transform: translateY(0);
                     transform-origin: top;
                     opacity: 1;
