24e2cd061f9bc1eab3e91c118fce9fa936d73083
Chano <github@chanovera.com>
2024-10-21 18:27:09 -0600

    ajustes a la página de archivo

--- functions/blog-css.php
+++ functions/blog-css.php
@@ -6,6 +6,7 @@ function renata_theme_custom_blog_css() {
         main .container{background-color:var(--bg-header);}
         main .container .section{padding:30px 0;display:grid;gap:50px;}
         main .container .section .posts-wrapper{display:grid;grid-template-rows:1fr auto;gap:50px;}
+        main .container .section .posts-wrapper > h1{grid-column:1/-1;}
         main .container .section .posts-wrapper .posts{display:grid;gap:50px;align-content:baseline;grid-template-columns:repeat(auto-fill, minmax(291px, 1fr));}
         main .container .section .posts-wrapper .posts .post{display:grid;gap:16px;align-content:baseline;}
         main .container .section .posts-wrapper .posts .post .thumbnail{width:100%;aspect-ratio:16/9;object-fit:cover;background-color:var(--bg-input);}
