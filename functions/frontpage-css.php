ab7275380009426cf2bf78b3db7d8006554e59b5
Chano <github@chanovera.com>
2024-10-22 08:43:36 -0600

    probando animación de entrada para las tarjetas de portafolio - tarjeta 1, 2 y 3

--- functions/frontpage-css.php
+++ functions/frontpage-css.php
@@ -157,7 +157,7 @@ function renata_theme_custom_frontpage_css(){
                 animation-timeline:scroll();
             }
             @keyframes portfolio-card-1 {
-                10%{transform:translateY(-60px);opacity:0;}
+                10%{transform:translateY(-80px);opacity:0;}
                 30%{transform:translateY(0);opacity:1;}
             }
             #portfolio .content .cards-portfolio .card-portfolio:nth-child(2){
@@ -165,16 +165,16 @@ function renata_theme_custom_frontpage_css(){
                 animation-timeline:scroll();
             }
             @keyframes portfolio-card-2 {
-                13%{transform:translateY(-60px);opacity:0;}
-                33%{transform:translateY(0);opacity:1;}
+                14%{transform:translateY(-80px);opacity:0;}
+                34%{transform:translateY(0);opacity:1;}
             }
             #portfolio .content .cards-portfolio .card-portfolio:nth-child(3){
                 animation: portfolio-card-3 both;
                 animation-timeline:scroll();
             }
             @keyframes portfolio-card-3 {
-                16%{transform:translateY(-60px);opacity:0;}
-                36%{transform:translateY(0);opacity:1;}
+                18%{transform:translateY(-80px);opacity:0;}
+                38%{transform:translateY(0);opacity:1;}
             }
             #portfolio .content .cards-portfolio .card-portfolio:hover{background-color:var(--bg-card-hover);}
             #portfolio .content .cards-portfolio .card-portfolio figure{position:relative;display:grid;gap:10px;height:100%;border-radius:6px;overflow:hidden;aspect-ratio:4/3;}
