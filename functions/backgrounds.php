7127b81eefa42bf7b51f54a8de3227875f717454
Chano <github@chanovera.com>
2024-10-23 15:16:11 -0600

    primeras secciones de los artículos, sus fondos, colores y primeras clases

--- functions/backgrounds.php
+++ functions/backgrounds.php
@@ -10,12 +10,12 @@ function renata_theme_custom_backgrounds() {
             <?php else: ?>
                 :is(.page-template-default,.post-template-default) .header-post{position:relative;background:url('<?php the_post_thumbnail_url( 'full' ); ?>');background-repeat:no-repeat;background-position:50% 50%;background-size:cover;}
                 :is(.page-template-default,.post-template-default) .header-post .blur{position:absolute;top:0;left:0;width:100%;height:100%;background:rgba(0,0,0,.4); backdrop-filter:saturate(180%) blur(20px); -webkit-backdrop-filter:saturate(180%) blur(20px);}
-                :is(.page-template-default,.post-template-default) .header-post .title-and-data__wrapper .title-post,
-                .post-template-default .header-post .title-and-data__wrapper .author .author-name,
-                .post-template-default .header-post .title-and-data__wrapper .author .author-ocupation,
-                .post-template-default .header-post .title-and-data__wrapper .date,
-                .post-template-default .header-post .title-and-data__wrapper .share,
-                .post-template-default .header-post .title-and-data__wrapper .share a{color:#fff;}
+                :is(.page-template-default,.post-template-default) main .header-post .title-and-data__wrapper .title-post,
+                .post-template-default main .header-post .title-and-data__wrapper .author .author-name,
+                .post-template-default main .header-post .title-and-data__wrapper .author .author-ocupation,
+                .post-template-default main .header-post .title-and-data__wrapper .date,
+                .post-template-default main .header-post .title-and-data__wrapper .share,
+                .post-template-default main .header-post .title-and-data__wrapper .share a{color:#fff;}
             <?php endif; ?>
 
         </style>
