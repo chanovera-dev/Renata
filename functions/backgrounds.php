0f81289cfe15893970f6cc566bc9247a235b6b1c
Chano <github@chanovera.com>
2024-10-23 14:09:00 -0600

    primeras secciones de los artículos, sus fondos, colores y primeras clases

--- functions/backgrounds.php
+++ functions/backgrounds.php
@@ -6,7 +6,7 @@ function renata_theme_custom_backgrounds() {
             <?php if ( has_post_thumbnail() == false ) : ?>
                 :is(.page-template-default,.post-template-default) .header-post{background-color:var(--bg-header);}
             <?php else: ?>
-                :is(.page-template-default,.post-template-default) .header-post{position:relative;background:url('<?php the_post_thumbnail_url( 'full' ); ?>');background-repeat:no-repeat;background-position:50% 50%;background-size:cover;color:#fff;}
+                :is(.page-template-default,.post-template-default) .header-post{position:relative;background:url('<?php the_post_thumbnail_url( 'full' ); ?>');background-repeat:no-repeat;background-position:50% 50%;background-size:cover;}
             <?php endif; ?>
 
         </style>
