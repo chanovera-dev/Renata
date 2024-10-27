3963d6aad9c4865ea4b51706f27faf650ba105fd
Chano <github@chanovera.com>
2024-10-27 15:44:13 -0600

    se especifica la aparición de las etiquetas en el loop dependiendo de si estáen la página de búsqueda, de archivo, blog, etc

--- templates/content-archive.php
+++ templates/content-archive.php
@@ -7,10 +7,13 @@
     ?>
     <a class="permalink" href="<?php the_permalink(); ?>" target="_blank"><?php the_title('<h3 class="permalink__title">', '</h3>'); ?></a>
     <?php 
-        the_excerpt(); 
+        the_excerpt();
+        
         if ( is_home() or is_page_template('home.php') or is_archive() && ! is_active_sidebar('posts-sidebar') ) {
             echo '<div class="tags">' . get_the_tag_list() . '</div>';
-        } else if ( is_search() && ! is_active_sidebar('search-sidebar') ) {
+        }
+
+        if ( is_search() && ! is_active_sidebar('search-sidebar') ) {
             echo '<div class="tags">' . get_the_tag_list() . '</div>'; 
         }
     ?>
