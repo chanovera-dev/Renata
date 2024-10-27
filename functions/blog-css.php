a03768c407fccb562d9e7bd44ffa775ea23b4f81
Chano <github@chanovera.com>
2024-10-27 13:10:14 -0600

    efecto de opacidad en la etiquetas del artículo que no tengan un hover

--- functions/blog-css.php
+++ functions/blog-css.php
@@ -11,6 +11,9 @@ function renata_theme_custom_blog_css() {
         main .container .section .posts-wrapper .posts .post .thumbnail{width:100%;aspect-ratio:16/9;object-fit:cover;background-color:var(--bg-input);border-radius:6px;}
         main .container .section .posts-wrapper .posts .post .permalink .permalink__title{}
         main .container .section .posts-wrapper .posts .post p{}
+        main .container .section .posts-wrapper .posts .post .tags{visibility:hidden;}
+        main .container .section .posts-wrapper .posts .post .tags > *{visibility:visible;transition:opacity .3s;}
+        main .container .section .posts-wrapper .posts .post .tags:hover > a:not(:hover){opacity:.25;}
         main .container .section .posts-wrapper .posts .post .tags a[rel="tag"]{padding: 3px 7px;background-color:var(--bg-tag-loop);border:1px solid var(--bg-tag-loop);border-radius:4px;color:var(--color-available-text);margin:0 7px 7px 0;font-size:13px;display:inline-block;transition:all .3s ease;}
         main .container .section .posts-wrapper .posts .post .tags a[rel="tag"]:hover{background-color:var(--bg-tag-loop-hover);border-color:var(--bg-tag-loop-hover);color:var(--resalted-text);}
 
