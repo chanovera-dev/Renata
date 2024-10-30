09e189a65cba2a549a458823b05ce08d9d58ef5f
Chano <github@chanovera.com>
2024-10-30 11:49:26 -0600

    colores de texto en el post cuando tiene fondo

--- functions/backgrounds.php
+++ functions/backgrounds.php
@@ -5,14 +5,13 @@ function renata_theme_custom_backgrounds() {
         <style>
             <?php if ( has_post_thumbnail() == false ) : ?>
                 :is(.page-template-default,.post-template-default) main #breadcrumb.container{background-color:var(--bg-input);border-top:1px solid var(--border-input);border-bottom:1px solid var(--border-input);}
-                :is(.page-template-default,.post-template-default) main #breadcrumb.container .top{padding:10px 0;}
                 :is(.page-template-default,.post-template-default) .header-post{background-color:var(--bg-header);}
                 :is(.page-template-default,.post-template-default) .header-post .blur{display:none;}
                 :is(.page-template-default,.post-template-default) .header-post .title-and-data__wrapper .title-post{color:var(--resalted-text);}
             <?php else: ?>
                 :is(.page-template-default,.post-template-default) .header-post{position:relative;background:url('<?php the_post_thumbnail_url( 'full' ); ?>');background-repeat:no-repeat;background-position:50% 50%;background-size:cover;}
                 :is(.page-template-default,.post-template-default) .header-post .blur{position:absolute;top:0;left:0;width:100%;height:100%;background:rgba(0,0,0,.4); backdrop-filter:saturate(180%) blur(20px); -webkit-backdrop-filter:saturate(180%) blur(20px);}
-                :is(.page-template-default,.post-template-default) main .header-post .top :is(.breadcrumb,.breadcrumb a,.category-item a),
+                :is(.page-template-default,.post-template-default) main .header-post :is(.top,.bottom) :is(.breadcrumb,.breadcrumb a,.category-item a,.date,.share),
                 :is(.page-template-default,.post-template-default) main .header-post .title-and-data__wrapper .title-post,
                 .post-template-default main .header-post .title-and-data__wrapper .author .author-name,
                 .post-template-default main .header-post .title-and-data__wrapper .author .author-ocupation,
