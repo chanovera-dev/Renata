3e5b5369f1f3017d3a9646ef4e610115c3bba309
Chano <github@chanovera.com>
2024-10-30 09:43:07 -0600

    cambios en el padding del contenedor de secciones

--- functions/blog-css.php
+++ functions/blog-css.php
@@ -12,7 +12,7 @@ function renata_theme_custom_blog_css() {
             #breadcrumb.container .section svg{height:1cap;}
         
         main .container{background-color:var(--bg-header);}
-        main .container .section{padding:30px 0;display:grid;gap:50px;}
+        main .container .section{padding:50px 0;display:grid;gap:50px;}
         main .container .section .posts-wrapper{display:grid;grid-template-rows:1fr auto;gap:50px;}
         main .container .section .posts-wrapper .posts{display:grid;gap:50px;align-content:baseline;grid-template-columns:repeat(auto-fill, minmax(291px, 1fr));}
         main .container .section .posts-wrapper .posts .post{display:grid;gap:16px;align-content:baseline;}
